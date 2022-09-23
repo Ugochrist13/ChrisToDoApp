61551f38ff0a6df6127d562fb7a564c1e1c1a4cc
Ugochrist13 <christianugoo@gmail.com>
2022-09-23 14:09:46 +0100

    update on to do list app

--- index.js
+++ index.js
@@ -14,26 +14,44 @@ if(localStorage.getItem("tasks") == null){
     let data = JSON.parse(localStorage.getItem("tasks"))
     tasks = data
     for(let i = 0; i < tasks.length; i++){
+        const index = i
         const newTask = document.createElement("li")
         const task = document.createElement("input")
         task.setAttribute("readonly", "readonly")
         task.value = tasks[i]
         newTask.appendChild(task)
+        const editCont = document.createElement("div")
+        editCont.setAttribute("class", "editCont")
         const editbtn = document.createElement("button")
         editbtn.innerText = "Edit"
-        editbtn.setAttribute("onclick", "editbtn()")
-        newTask.appendChild(editbtn)
+        editbtn.setAttribute("id", "edit")
+        editCont.appendChild(editbtn)
         const delbtn = document.createElement("button")
         delbtn.innerText = "Delete"
-        delbtn.setAttribute("onclick", "del(this,"+ (tasks.length-1) +")")
-        newTask.appendChild(delbtn)
-        tasks.push(input.value)
+        delbtn.setAttribute("class", "del")
+        delbtn.setAttribute("onclick", "del(this, index)")
+        editCont.appendChild(delbtn)
+        newTask.appendChild(editCont)
         taskList.appendChild(newTask)
+
+        editbtn.addEventListener("click", function(e){
+            if(editbtn.innerText == "Edit"){
+                editbtn.innerText = "Save"
+                task.removeAttribute("readonly")
+                task.focus()
+            }else if(editbtn.innerText == "Save"){
+                editbtn.innerText = "Edit"
+                task.setAttribute("readonly", "readonly")
+                tasks[index] = task.value
+                let data = JSON.stringify(tasks)
+                localStorage.setItem("tasks", data)
+            }
+        })
 
         if(tasks !== "[]"){
             num.innerText = tasks.length
         }
-        if(num.innerText !== "" && num.innerText !== "0" && num.innerText !== "1"){
+        if(num.innerText != "" && num.innerText !== "0" && num.innerText !== "1"){
             plural.innerText = "s"
         }else{
             plural.innerText = ""
@@ -80,35 +98,40 @@ clearbtn.addEventListener("click", function(e){
     }
 })
 
-addbtn.addEventListener("click", function(e){
+addbtn.addEventListener("click", function(e, index){
     e.preventDefault()
     if(input.value != ""){
         const newTask = document.createElement("li")
         const task = document.createElement("input")
         task.setAttribute("readonly", "readonly")
         task.value = input.value
+        tasks.push(input.value)
         newTask.appendChild(task)
+        const editCont = document.createElement("div")
+        editCont.setAttribute("class", "editCont")
         const editbtn = document.createElement("button")
         editbtn.innerText = "Edit"
-        newTask.appendChild(editbtn)
+        editbtn.setAttribute("id", "edit")
+        editCont.appendChild(editbtn)
         const delbtn = document.createElement("button")
         delbtn.innerText = "Delete"
-        delbtn.setAttribute("onclick", "del(this,"+ (tasks.length-1) +")")
-        newTask.appendChild(delbtn)
-        tasks.push(input.value)
+        delbtn.setAttribute("class", "del")
+        delbtn.setAttribute("onclick", "del(this, index)")
+        editCont.appendChild(delbtn)
+        newTask.appendChild(editCont)
         taskList.appendChild(newTask)
 
         editbtn.addEventListener("click", function(e){
             if(editbtn.innerText == "Edit"){
                 editbtn.innerText = "Save"
                 task.removeAttribute("readonly")
-                task.focus
-                tasks[e] = task(e)
-                let data = JSON.stringify(tasks)
-                localStorage.setItem("tasks", data)
+                task.focus()
             }else if(editbtn.innerText == "Save"){
                 editbtn.innerText = "Edit"
                 task.setAttribute("readonly", "readonly")
+                tasks[index] = task.value
+                let data = JSON.stringify(tasks)
+                localStorage.setItem("tasks", data)
             }
         })
 
